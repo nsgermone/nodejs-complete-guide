6f401d7b0a53cb51d3445ee925f6c3d514e2fd77
nsgermone <nicolas.germone@externos-ar.cencosud.com>
2022-06-07 13:32:20 -0300

    Fixed localhost

--- routes.js
+++ routes.js
@@ -1,39 +1,42 @@
 const fs = require('fs');
 
 const requestHandler = (req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    // It allows us to listen to certain events, I want to listen for the data event
+    // then the function to execute for every data event
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
-      }
-      if (url === '/message' && method === 'POST') {
-        const body = [];
-        // It allows us to listen to certain events, I want to listen for the data event
-        // then the function to execute for every data event
-        req.on('data', (chunk) => {
-          body.push(chunk);
-        });
-        req.on('end', () => {
-          const parsedBody = Buffer.concat(body).toString();
-          const message = parsedBody.split('=')[1];
-          fs.writeFile('message.txt', message, (err) => {
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
-          });
-        });
-      }
-      res.setHeader('Content-Type', 'text/html');
-      res.write('<html>');
-      res.write('<head><title>My First Page</title><head>');
-      res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-      res.write('</html>');
-      res.end();
-}
+      });
+    });
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+};
 
   module.exports = {
       handler: requestHandler,
